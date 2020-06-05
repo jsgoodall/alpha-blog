4d1870b58d659a5673b34c9b469fc2db034aac6f
Simon Goodall <j.simon.goodall@gmail.com>
2020-06-06 00:43:52 +0200

    added helper-methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
   end
@@ -15,11 +15,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successuflly updated"
       redirect_to @user
@@ -31,6 +29,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have succesfully signed up"
       redirect_to articles_path
     else
@@ -39,6 +38,10 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,23 @@ module ApplicationHelper
   def gravatar_for( user, options = { size: 80})
     # get the email from URL-parameters or what have you and make lowercase
     email_address = user.email.downcase
-     
+
     # create the md5 hash
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
-        
+
     # compile URL which can be used in <img src="RIGHT_HERE"...
     image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
 
     image_tag(image_src, alt: user.username, class: "rounded mx-auto d-block align-content-center shadow")
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
   end
 
 end
